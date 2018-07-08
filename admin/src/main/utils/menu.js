f3421aeeca8ff3f29004b130dfa1cce00885e678
joao <joao.neto.ninja@hotmail.com>
2018-07-07 22:56:49 -0300

    alterando ícones do header

--- admin/src/main/utils/menu.js
+++ admin/src/main/utils/menu.js
@@ -10,23 +10,23 @@ class Menu extends Component {
                         <Link to="/"><i className="mdi mdi-view-dashboard"></i> <span> Dashboard </span> </Link>
                     </li>
                     <li className="has-submenu">
-                        <Link to="/tickets"><i className="mdi mdi-invert-colors"></i> <span> Tickets </span> </Link>
+                        <Link to="/tickets"><i className="mdi mdi-av-timer"></i> <span> Tickets </span> </Link>
                     </li>
 
                     <li className="has-submenu">
-                        <Link to="/services"><i className="mdi mdi-texture"></i><span> Serviços </span> </Link>
+                        <Link to="/services"><i className="mdi mdi-screwdriver"></i><span> Serviços </span> </Link>
                     </li>
 
                     <li className="has-submenu">
-                        <Link to="/sub_services"><i className="mdi mdi-texture"></i><span> Sub Serviços </span> </Link>
+                        <Link to="/sub_services"><i className="mdi mdi-wrench"></i><span> Sub Serviços </span> </Link>
                     </li>
 
                     <li className="has-submenu">
-                        <Link to="/organizations"><i className="mdi mdi-texture"></i><span> Organizações </span> </Link>
+                        <Link to="/organizations"><i className="mdi mdi-home-assistant"></i><span> Organizações </span> </Link>
                     </li>
 
                     <li className="has-submenu">
-                        <Link to="/agents"><i className="mdi mdi-texture"></i><span> Operadores </span> </Link>
+                        <Link to="/agents"><i className="mdi mdi-worker"></i><span> Operadores </span> </Link>
                     </li>
                 </ul>
             </div>
